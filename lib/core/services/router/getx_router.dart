f3b01696e3dd76fe4a66a4c9b5a7d2eb1e2f40de
Bernardo Veras <bernardo@vvssistemas.com.br>
2021-11-27 03:40:21 -0300

    Add propertie for go to route and exclude previous route

--- lib/core/services/router/getx_router.dart
+++ lib/core/services/router/getx_router.dart
@@ -5,16 +5,17 @@ import '../../routes.dart';
 
 class GetXInteraRouter extends GetxService implements InteraRouter {
   @override
-  Future<T?>? to<T>(String route, {dynamic arguments}) => Get.toNamed<T>(route, arguments: arguments);
+  Future<T?>? to<T>(String route, {bool closeAll = false, dynamic arguments}) =>
+      closeAll == true ? Get.offAndToNamed(route, arguments: arguments) : Get.toNamed<T>(route, arguments: arguments);
 
   @override
-  Future<T?>? toLogin<T>({String route = InteraRoutes.LOGIN}) => to<T>(route);
+  Future<T?>? toLogin<T>({String route = InteraRoutes.LOGIN, bool closeAll = true}) => to<T>(route, closeAll: closeAll);
 
   @override
-  Future<T?>? toHome<T>({String route = InteraRoutes.HOME}) => to<T>(route);
+  Future<T?>? toHome<T>({String route = InteraRoutes.HOME, bool closeAll = false}) => to<T>(route, closeAll: closeAll);
 
   @override
-  Future<T?>? toSettings<T>({String route = InteraRoutes.SETTINGS}) => to<T>(route);
+  Future<T?>? toSettings<T>({String route = InteraRoutes.SETTINGS, bool closeAll = false}) => to<T>(route, closeAll: closeAll);
 }
 
 InteraRouter get router => Get.find<InteraRouter>();
--- lib/core/services/router/router.dart
+++ lib/core/services/router/router.dart
@@ -1,8 +1,8 @@
 import '../../routes.dart';
 
 abstract class InteraRouter {
-  Future<T?>? to<T>(String route, {dynamic arguments});
-  Future<T?>? toLogin<T>({String route = InteraRoutes.LOGIN});
-  Future<T?>? toHome<T>({String route = InteraRoutes.HOME});
-  Future<T?>? toSettings<T>({String route = InteraRoutes.SETTINGS});
+  Future<T?>? to<T>(String route, {bool closeAll = false, dynamic arguments});
+  Future<T?>? toLogin<T>({String route = InteraRoutes.LOGIN, bool closeAll = true});
+  Future<T?>? toHome<T>({String route = InteraRoutes.HOME, bool closeAll = false});
+  Future<T?>? toSettings<T>({String route = InteraRoutes.SETTINGS, bool closeAll = false});
 }
