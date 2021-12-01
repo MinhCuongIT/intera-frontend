7df4d01dc3973d5e39596b2e428caea3a675d659
Bernardo Veras <bernardo@vvssistemas.com.br>
2021-11-30 23:21:16 -0300

    Add local storage keys constants

---
+++ lib/core/constants/local_storage_keys.dart
@@ -0,0 +1,7 @@
+class LocalStorageKeys {
+  const LocalStorageKeys._();
+
+  static const String lastLoginDate = 'last_login_date';
+  static const String lastLoginType = 'last_login_type';
+  static const String loggedUserInfo = 'logged_user_info';
+}
